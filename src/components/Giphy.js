ff92a748ee812eb8ba76d91c64f42292a4c3bfa6
Anshuljain05 <anshuljain05@outlook.com>
2022-07-24 13:18:50 +0530

    Added Loader Spinner

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Loader from './Loader';
 
 
 function Giphy() {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
+        setIsLoading(true);
         const fetchData = async () => {
             const results = await axios("https://api.giphy.com/v1/gifs/trending", {
                 params: {
@@ -13,12 +16,25 @@ function Giphy() {
             });
             console.log(results);
             setData(results.data.data);
+            setIsLoading(false);
         }
         fetchData();
     }, [])
     
+    const renderGifs = () => {
+        if (isLoading) {
+            return <Loader/>
+        }
+        return data.map(ele => {
+            return (
+                <div key={ele.id} className='gif'>
+                    <img src={ele.images.fixed_height.url} alt=""></img>
+                </div>
+            )
+        })
+    } 
     return (
-        <div className=''>Giphy</div>
+        <div className='container gifs'>{renderGifs()}</div>
     )
 }
 
